cdbe31462b00a08a4aed8f040d8226c7be5bd024
Sathes Balasingam <sathes352@gmail.com>
2017-01-27 15:27:03 -0500

    done

--- contact.rb
+++ contact.rb
@@ -14,6 +14,24 @@ class Contact < ActiveRecord::Base
   def full_name
     "#{first_name} #{last_name}"
   end
+
+  def setup
+  @contact = Contact.create(
+    first_name: 'Grace',
+    last_name:  'Hopper',
+    email:      'grace@hopper.com',
+    note:       'computer scientist')
+end
+
+def test_update
+  @contact.update(note: 'wrote the first compiler in 1952')
+  assert_equal 'wrote the first compiler in 1952', @contact.note
+end
+
+def test_find_by
+  assert_equal @contact, Contact.find_by(first_name: 'Grace')
+end
+
 end
 
 
